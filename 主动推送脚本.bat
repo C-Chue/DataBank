32ab04391ea86b8e7d50d4d2fef31dcf953d4bce
C-Chue <zzihuachue@gmail.com>
2023-11-26 23:12:10 +0800

    'test'

--- 主动推送脚本.bat
+++ 主动推送脚本.bat
@@ -4,7 +4,7 @@ chcp 65001
 echo + ------------------------------------------- +
 echo + -- 主动推送当前文件夹内容 --> github -- +
 git add * 
-git commit -m '主动推送'
+git commit -m '%date% %time%-主动推送'
 git push origin main
 echo + ---------------- 推送成功 ----------------- +
 
