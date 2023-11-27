8e4b2cd820758dd48d2c8a11018fab832fa68e27
C-Chue <zzihuachue@gmail.com>
2023-11-27 19:46:11 +0800

    周一 2023/11/27 19:46:11.82主动推送

--- 主动推送脚本.bat
+++ 主动推送脚本.bat
@@ -5,7 +5,7 @@ echo + ------------------------------------------- +
 echo + -- 主动推送当前文件夹内容 --> github -- +
 
 git add * 
-git commit -m '主动推送'
+git commit -m "%date% %time%主动推送"
 git push origin main
 
 echo + ---------------- 推送成功 ----------------- +
